50152d71b011db82389cafbf6669bcb4c75e193d
juanma <arcejuanma@gmail.com>
2019-11-06 16:16:32 -0300

    Esqueleto Hecho

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,32 +1,20 @@
 import React, { Component } from "react";
 import List from "../components/List";
 import axios from "axios";
+import { Route, Redirect, Switch, BrowserRouter, withRouter } from 'react-router-dom';
 import Register from '../components/Register'
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      list: [] //array of user objects
-    };
-  }
-
-
-
-  componentDidMount() {
-    //TEMPLATE ON HOW TO MAKE API REQUESTS TO BACKEND
-    axios.get("/api/users").then(response => {
-      this.setState({ list: response.data });
-    });
   }
 
   render() {
     return (
       <div>
-        <Register />
-        LISTA DE USUARIOS
-        <List list={this.state.list} />
-     
+      <Switch>
+
+      </Switch>
       </div>
     );
   }
