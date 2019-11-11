d8fe8956abee38c306a6d97abceb57cae3c673c8
faculafuente7 <faculafuente@yahoo.com>
2019-11-11 09:27:17 -0300

    commit

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -21,7 +21,8 @@ class ProductDetails extends Component {
         <h1>{product.name}</h1>
         <img src={product.imgURL} />
         <h3>$ {product.price} </h3>
-        <p>Synopsis {product.description} </p>
+        <p>
+          {product.description} </p>
       </div>
     );
   }
