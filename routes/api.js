38cca45d5d2e4a22cb4563557a2c22f757e72837
NJAbuin <nabuin@netuniversecorp.com>
2019-11-14 18:23:16 -0300

    prettier

--- routes/api.js
+++ routes/api.js
@@ -99,24 +99,35 @@ api.get("/categs/:cat", (req, res) => {
   Book.findByCategory(req.params.cat).then(data => res.send(data));
 });
 
-api.post('/removeFromCart', (req,res)=>{
-  const add = req.body
-  Cart.findOne({where: { cartId: add.userId, state: "Opened" } })
-  .then(e => {return e})
-  .then(e=>{
-    CartProduct.destroy({where:{
-    orderId: e.id,
-    bookId: add.book,}
-    //cartId: e.cartId}
-  })})
-  .then(()=>  Cart.findAll({where:{cartId: user.userId, state:"Opened"}, include:[{all:true}]}))
-  .then(e=>{
-    if(e != undefined) res.send(e[0].dataValues.books)
-    else {res.send(null)}
-  })
-  .catch(err=>console.log(err))
-})
-
+api.post("/removeFromCart", (req, res) => {
+  const add = req.body;
+  Cart.findOne({ where: { cartId: add.userId, state: "Opened" } })
+    .then(e => {
+      return e;
+    })
+    .then(e => {
+      CartProduct.destroy({
+        where: {
+          orderId: e.id,
+          bookId: add.book
+        }
+        //cartId: e.cartId}
+      });
+    })
+    .then(() =>
+      Cart.findAll({
+        where: { cartId: user.userId, state: "Opened" },
+        include: [{ all: true }]
+      })
+    )
+    .then(e => {
+      if (e != undefined) res.send(e[0].dataValues.books);
+      else {
+        res.send(null);
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 api.post("/addToCart", (req, res) => {
   const add = req.body;
@@ -127,7 +138,7 @@ api.post("/addToCart", (req, res) => {
     .then(e => {
       return (cartData = { cartId: e[0].cartId, orderId: e[0].id });
     })
-  //Busca el libro para el numero de orden y el usuario (cartId)
+    //Busca el libro para el numero de orden y el usuario (cartId)
     .then(cart => {
       return CartProduct.findOne({
         where: {
@@ -137,38 +148,41 @@ api.post("/addToCart", (req, res) => {
         }
       });
     })
-    .then(e => {  
-      console.log('Llegue hasta justo antes de crear o actualizar')
-      if(e==null){
+    .then(e => {
+      console.log("Llegue hasta justo antes de crear o actualizar");
+      if (e == null) {
         CartProduct.create({
-          //cartId: cartData.cartId,
+          cartId: cartData.cartId,
           bookId: add.bookId,
           quantity: add.quantity,
           orderId: cartData.orderId
-        })
-        return null
-      }
-      else{
+        });
+        return null;
+      } else {
         CartProduct.update(
           { quantity: add.quantity },
           {
             where: {
-              //cartId: cartData.cartId,
+              cartId: cartData.cartId,
               // id: add.userId,
               orderId: cartData.orderId,
               bookId: add.bookId
             }
           }
-        )
-        return null
+        );
+        return null;
       }
     })
-    .then(()=> {
-      return Cart.findAll({where:{cartId: add.userId, state:"Opened"}, include:[{all:true}] })}) //, include:[{all:true}]
-    .then(e=>{
-      res.send(e)})
-    .catch(err => console.log(err))
-    
+    .then(() => {
+      return Cart.findAll({
+        where: { cartId: add.userId, state: "Opened" },
+        include: [{ all: true }]
+      });
+    }) //, include:[{all:true}]
+    .then(e => {
+      res.send(e);
+    })
+    .catch(err => console.log(err));
 });
 
 api.get("/cart", (req, res) => {
@@ -180,17 +194,19 @@ api.post("/addToCartinBulkReplace", (req, res) => {
   // Chequeo que no haya ningun carro del usuario abierto y si lo tiene se lo cierro
   Cart.update(
     { state: "Droped" },
-    { where: { cartId: add.userId, state: "Opened" } })
+    { where: { cartId: add.userId, state: "Opened" } }
+  )
     .then(() => Cart.create({ cartId: add.userId, state: "Opened" }))
     .then(e =>
-      add.bookId.map(book =>{
+      add.bookId.map(book => {
         CartProduct.create({
           orderId: e.id,
           bookId: book.id,
-          quantity: book.quantity,
+          quantity: book.quantity
           //cartId: e.cartId
-        })
-      }))
+        });
+      })
+    )
     .catch(err => console.log(err));
 });
 
@@ -208,7 +224,7 @@ api.post("/addToCartinBulkMerge", (req, res) => {
         CartProduct.findOne({
           where: {
             orderId: e.id,
-            bookId: book.id,
+            bookId: book.id
             //cartId: e.cartId
           }
         }).then(res => {
@@ -217,12 +233,12 @@ api.post("/addToCartinBulkMerge", (req, res) => {
             ? CartProduct.create({
                 orderId: e.id,
                 bookId: book.id,
-                quantity: book.quantity,
+                quantity: book.quantity
                 //cartId: e.cartId
               })
             : CartProduct.update(
                 { quantity: res.quantity + book.quantity },
-                { where: { orderId: e.id, bookId: book.id} }
+                { where: { orderId: e.id, bookId: book.id } }
               );
         })
       )
@@ -239,19 +255,21 @@ api.post("/addToCartinBulkMerge", (req, res) => {
 
 api.post("/getNumberOfCarts", (req, res) => {
   user = req.body;
-  Cart.findOne({where:{cartId: user.userId, state:"Opened"}, include:[{all:true}]})
-  .then(e=>{
-    console.log(e)
-    if(e != null) {
-      console.log('holaaaaa', e.dataValues.books)
-      res.send(e.dataValues.books)}
-    else {res.send(null)}
+  Cart.findOne({
+    where: { cartId: user.userId, state: "Opened" },
+    include: [{ all: true }]
   })
     .then(e => {
-      if (e != undefined) res.send(e[0].dataValues.books);
-      else {
+      console.log(e);
+      if (e != null) {
+        console.log("holaaaaa", e.dataValues.books);
+        res.send(e.dataValues.books);
+      } else {
         res.send(null);
       }
+    })
+    .then(e => {
+      if (e != undefined) res.send(e[0].dataValues.books);
     })
     .catch(console.error());
 });
